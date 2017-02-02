73c80175bc8e2d57d3170ed1f5c2098a33934b3d
jkhoang313 <jkhoang313@gmail.com>
2017-02-02 15:37:10 -0500

    Done.

--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -4,30 +4,29 @@ const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
   render() {
-    // var array = []
-    // this.props.pets.forEach((pet) => {
-    //   array.push(pet)
-    // }
-    //
-    // return (
-    //   <div className="ui cards">
-    //     {array}
-    //   </div>
-    // )
+    var array = this.props.pets.map((pet) => {
+      array.push(pet)
+    }
+
+    return (
+      <div className="ui cards">
+        {array}
+      </div>
+    )
 
     {
-   var nameslist = []
-   let self = this
-   self.props.pets.forEach((pet)=>{
-     if(self.props.adoptedPets.includes(pet.id)){
-       nameslist.push(<div className="ui cards"><code> <Pet pet={pet} isAdopted={true} onAdoptPet={this.props.onAdoptPet}/> </code></div>)
-     }else{
-       nameslist.push(<div className="ui cards"><code> <Pet pet={pet} isAdopted={false} onAdoptPet={this.props.onAdoptPet}/> </code></div>)
-     }
-
-   })
-   return <div>{ nameslist }</div>
- }
+ //   var nameslist = []
+ //   let self = this
+ //   self.props.pets.forEach((pet)=>{
+ //     if(self.props.adoptedPets.includes(pet.id)){
+ //       nameslist.push(<div className="ui cards"><code> <Pet pet={pet} isAdopted={true} onAdoptPet={this.props.onAdoptPet}/> </code></div>)
+ //     }else{
+ //       nameslist.push(<div className="ui cards"><code> <Pet pet={pet} isAdopted={false} onAdoptPet={this.props.onAdoptPet}/> </code></div>)
+ //     }
+ //
+ //   })
+ //   return <div>{ nameslist }</div>
+ // }
   }
 }
 
